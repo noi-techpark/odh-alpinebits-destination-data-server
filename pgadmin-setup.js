ff4bb610a249cade67b7a825b261dd3250396ee0
João Carlos de A. R. Gonçalves <jcgon21@gmail.com>
2021-10-12 20:27:22 -0300

    Add a nodejs script to create a servers.json config file
    Added a nodejs script to create a servers.json config file, in order to mke pgadmin able to start with the correct postgresql db setup configuration

---
+++ pgadmin-setup.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+require('dotenv').config()
+const filepath = './pgadmin-servers.json';
+let servers = {
+    "Servers": {
+        "test": {
+            "Name": process.env.DB_NAME,
+            "Group": "Servers",
+            "Port": process.env.DB_PORT,
+            "Username": process.env.DB_USERNAME,
+            "Host": process.env.DB_HOSTNAME,
+            "SSLMode": "prefer",
+            "MaintenanceDB": "postgres"
+        }
+    }
+}
+let servers_data = JSON.stringify(servers, null, 2);
+fs.writeFileSync(filepath, servers_data);
